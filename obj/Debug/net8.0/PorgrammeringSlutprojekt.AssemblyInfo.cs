0f96196e49cc6713cc61dd689fadfcb7d9156e25
Rittza <elijah.b.rylander@gmail.com>
2024-05-30 22:08:58 +0200

    Färdig
    asdf

--- obj/Debug/net8.0/PorgrammeringSlutprojekt.AssemblyInfo.cs
+++ obj/Debug/net8.0/PorgrammeringSlutprojekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PorgrammeringSlutprojekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c39044a0d5f6c7249d66e857f2338ed5f78329a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1fee9b2b3006f63870209b96d3c370937f4c23c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PorgrammeringSlutprojekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PorgrammeringSlutprojekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
